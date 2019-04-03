885e79e0997519c361f4a04051fada066e0b0feb
anava4 <anava4@mail.greenriver.edu>
2019-04-03 17:36:17 -0400

    created a function that prints an array

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 1</title>
+
+</head>
+<body>
+    <header>Pair Program 1</header>
+
+</body>
+</html>
+
+<?php
+
+$numbers = array (7,9,8,9,8,8,6);
+function printArr($numbers)
+{
+    foreach($numbers AS $arr){
+        echo '<pre>'; print_r($arr); '</pre>';
+    }
+}
+printArr($numbers);
+
+
+?>
